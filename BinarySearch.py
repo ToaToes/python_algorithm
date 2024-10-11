36d691dd28976647c05cac47e6a3dfd21efbe09e
Tom Chen <33406671+ToaToes@users.noreply.github.com>
2024-10-11 18:05:46 -0300

    Create BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,50 @@
+'''
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+
+Use a binary search to solve, have one or two pivot to track
+'''
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums)-1
+
+        #eliminate the biggest or smallest case
+        if(nums[right] < target):
+            return right+1
+        elif(nums[left] > target):
+            return left
+
+        #update the pivot for track down in-between case
+        while left < right:
+            mid = (right + left)//2 #to get the left flooring num
+
+            if(right - left == 1):
+                return right
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid
+            elif nums[mid] < target:
+                left = mid
